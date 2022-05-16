491811fee581ac50f28707b25b72bb4a998addc3
plpallavi <pallavi121202@gmail.com>
2022-05-16 14:51:53 +0530

    DSA

--- Lecture09/30_MaxMininArr.cpp
+++ Lecture09/30_MaxMininArr.cpp
@@ -1,33 +1,42 @@
 #include<iostream>
 using namespace std;
 
-int getMax (int num[],int n){
-	int max = INT_MIN;
+int getMin(int num[],int n){
+	int mini = INT_MAX;
 	
 	for(int i=0; i<n; i++){
+		
+		mini = min(mini, num[i]);
+		
+		/*
 		if(num[i]>max){
 			max=num[i];
 		}
+		*/
 	}
 	
-	// Returning max value
-	return max;
+	// Returning min value
+	return mini;
 }
 
-/*
+
 int getMax (int num[],int n){
-	int max = INT_MIN;
+	int maxi = INT_MIN;
 	
 	for(int i=0; i<n; i++){
+		maxi = max(maxi,num[i]);
+		
+		/*
 		if(num[i]>min){
 			max=num[i];
 		}
+		*/
 	}
 	
 	// Returning max value
-	return max;
+	return maxi;
 }
-*/
+
 
 int main(){
 	int size;
@@ -42,7 +51,7 @@ int main(){
 	}
 	
 	cout<<"Maximum Value is "<<getMax(num,size)<<endl;
-	//cout<<"Minimum Value is "<<getMin(num,size)<<endl;
+	cout<<"Minimum Value is "<<getMin(num,size)<<endl;
 	
 	return 0;
 }
